f56c458be7aa8ae7403fcdc4657b60243e3925f6
Patrick E <github@evenet.ca>
2026-03-06 17:48:17 -0330

    moved variables to env file

--- digest/schedule.py
+++ digest/schedule.py
@@ -1,19 +1,30 @@
+import os
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 from pytz import timezone
 
 from digest.app import scrape, summarize
 
-scheduler = BlockingScheduler(timezone=timezone("America/St_Johns"))
+from dotenv import load_dotenv
 
-@scheduler.scheduled_job('cron', hour=17, minute=40)
+load_dotenv()
+
+t_zone = os.getenv("TIMEZONE")
+scrape_schedule_hour = os.getenv("SCRAPE_SCHEDULE_HOUR")
+scrape_schedule_minute = os.getenv("SCRAPE_SCHEDULE_MINUTE")
+summarize_schedule_hour = os.getenv("SUMMARIZE_SCHEDULE_HOUR")
+summarize_schedule_minute = os.getenv("SUMMARIZE_SCHEDULE_MINUTE")
+
+scheduler = BlockingScheduler(timezone=timezone(t_zone))
+
+@scheduler.scheduled_job('cron', hour=scrape_schedule_hour, minute=scrape_schedule_minute)
 def scrape_job():
     print("Starting scrape job...")
     scrape()
 
-@scheduler.scheduled_job('cron', hour=17, minute=45)
+@scheduler.scheduled_job('cron', hour=summarize_schedule_hour, minute=summarize_schedule_minute)
 def summarize_job():
-    print("Starting scrape job...")
+    print("Starting summarize job...")
     summarize()
-
 
 scheduler.start()
