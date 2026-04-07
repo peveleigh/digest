451eceb1dc80bb90c2cb00af7b7a1b74041c526e
Patrick Eveleigh <patrick@evenet.ca>
2026-04-07 22:06:36 +0000

    Added additional skip pattern

--- digest/news_scraper.py
+++ digest/news_scraper.py
@@ -28,7 +28,7 @@ class NewsScraper:
         # Constants for filtering
         self.URL_SKIP_PATTERNS = [
             "/livestory/","/video/","/sports/","/tv-shows/","/player/","/radio/",
-            "/music/","/entertainment/","/liveblog/","/commentary/"
+            "/music/","/entertainment/","/liveblog/","/commentary/","live"
         ]
         self.URL_REQUIRE_PATTERNS = ["www.cbc.ca/news/"]
 
